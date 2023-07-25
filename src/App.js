d314d85ef4ed0871c761972120c16c9450c38b7e
hbin1911 <rajas.mulik@gmail.com>
2023-07-25 18:03:52 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@
 function App() {
   return (
     <div>
-   
+    {/* <div>
+      Phone Directory
+    </div>
+    <button> Add </button>
+    <div>
+      <span> Name </span><br/>
+      <span> Phone </span>
+    </div> */}
+    <label htmlFor="name">Name: </label>
+    <input type="text" id="name" placeholder="Type here" defaultValue="Rajas"></input>
     </div>
   );
 }
