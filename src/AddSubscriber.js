7c00e53150a29e6a292394cff318a36fb42f0605
hbin1911 <rajas.mulik@gmail.com>
2023-07-29 00:49:10 +0530

    Added 'AddSubscriber' component

---
+++ src/AddSubscriber.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import './AddSubscriber.css';
+
+class AddSubscriber extends Component{
+    render(){
+        return(
+            <div>
+                <Header title="Add Subscriber"/>
+                <div className="component-body-container">
+                    <button className='custom-btn'>Back</button>
+                    <form className='subscriber-form'>
+                        <label htmlFor='name' className='label-control'>Name:</label><br/>
+                        <input type='text' id='name' name='name' className='input-control'/><br/><br/>
+                        <label htmlFor='phone' className='label-control'>Phone:</label><br/>
+                        <input type='text' id='phone' name='phone' className='input-control'/><br/><br/>
+                        <div className='subscriber-info-container'>
+                            <span className='subscriber-to-add-heading'>Subscriber to be added </span><br/>
+                            <span className='subscriber-info'>Name: </span><br/>
+                            <span className='subscriber-info'>Phone: </span>
+
+
+                        </div>
+                        <button className='custom-btn add-btn'>Add</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AddSubscriber;
